a359d21ed3b2e22119f447d1f0f42b3feddc018d
Elis Axelsson <elis.axelsson@gmail.com>
2015-08-03 13:19:58 +0200

    Renamed the layout from 0bl-mode to 0blayout to follow emacs best practices for minor modes

--- 0blayout.el
+++ 0blayout.el
@@ -1,14 +1,14 @@
-;;; 0bl-mode.el --- Layout grouping with ease
+;;; 0blayout.el --- Layout grouping with ease
 
 ;; Copyright (c) 2015 Elis Axelsson
 
 ;; Author: Elis "etu" Axelsson
-;; URL: https://github.com/etu/0bl-mode
+;; URL: https://github.com/etu/0blayout
 ;; Version: 0.1
 
 ;;; Commentary:
 
-;; The project is hosted at https://github.com/etu/0bl-mode
+;; The project is hosted at https://github.com/etu/0blayout
 ;; The latest version, and all the relevant information can be found there.
 
 ;;; License:
@@ -33,25 +33,25 @@
 ;; Put this file in your Emacs lisp path (eg site-lisp) and add to this
 ;; to your .emacs file:
 ;;
-;; (require '0bl-mode)
-;; (0bl-mode)
+;; (require '0blayout)
+;; (0blayout)
 
 ;;; Code:
 
-(defvar 0bl-layout-alist ()
+(defvar 0blayout-alist ()
   "List of the currently defined layouts.")
-(defvar 0bl-current-layout "default"
+(defvar 0blayout-current "default"
   "Currently active layout")
 
 
 
 ;; Function to create a new layout
-(defun 0bl-new-layout (layout-name)
+(defun 0blayout-new (layout-name)
   "0blayout creating function, default keybind for this function is C-c C-l C-c"
   (interactive "sEnter name of new layout: ")
 
   ;; Save the currently active layout
-  (0bl-save-layout)
+  (0blayout-save)
 
   ;; Then we just delete all other windows and switch to a *scratch* buffer,
   ;; then it's up to the user to set up their layout.
@@ -59,45 +59,45 @@
   (find-file "*scratch*")
 
   ;; Save the name of the new current layout
-  (setq 0bl-current-layout layout-name))
+  (setq 0blayout-current layout-name))
 
 
 
 ;; Function to kill current layout
-(defun 0bl-kill-layout ()
+(defun 0blayout-kill ()
   "0blayout removal function, default keybind for this function is C-c C-l C-k"
   (interactive)
 
-  (message "Killing layout: '%s'" 0bl-current-layout)
+  (message "Killing layout: '%s'" 0blayout-current)
 
   ;; Remove current layout from known layouts
-  (setq 0bl-layout-alist
-        (assq-delete-all (intern 0bl-current-layout) 0bl-layout-alist))
+  (setq 0blayout-alist
+        (assq-delete-all (intern 0blayout-current) 0blayout-alist))
 
   ;; Switch to next layout in the list
-  (let ((new-layout (car (car 0bl-layout-alist))))
+  (let ((new-layout (car (car 0blayout-alist))))
     (if (eq new-layout nil)
         ;; If there's no other layout, make a new default layout
         (progn
-          (setq 0bl-current-layout "default")
-          (0bl-new-layout "default"))
+          (setq 0blayout-current "default")
+          (0blayout-new "default"))
 
       ;; Switch to some other saved layout
       (progn
-        (set-window-configuration (cdr (car 0bl-layout-alist)))
-        (setq 0bl-current-layout (symbol-name new-layout))))))
+        (set-window-configuration (cdr (car 0blayout-alist)))
+        (setq 0blayout-current (symbol-name new-layout))))))
 
 
 
 ;; Function to switch layout
-(defun 0bl-switch-layout (layout-name)
+(defun 0blayout-switch (layout-name)
   "0blayout switching function, default keybind for this function is C-c C-l C-b"
   (interactive "sEnter name of layout to switch to: ")
 
   ;; Save the currently active layout
-  (0bl-save-layout)
+  (0blayout-save)
 
-  (let ((layout (assoc (intern layout-name) 0bl-layout-alist)))
+  (let ((layout (assoc (intern layout-name) 0blayout-alist)))
     (if (eq layout nil)
         (message "No layout with name: '%s' is defined" layout-name)
       (progn
@@ -105,41 +105,41 @@
         (set-window-configuration (cdr layout))
 
         ;; Save the name of the currently active layout
-        (setq 0bl-current-layout layout-name)
+        (setq 0blayout-current layout-name)
 
         (message "Switch to layout: '%s'" layout-name)))))
 
 
 
 ;; Function to save layout
-(defun 0bl-save-layout ()
+(defun 0blayout-save ()
   "This is a helper function to save the current layout."
 
   ;; Remove all saves of current layout before saving
-  (setq 0bl-layout-alist
+  (setq 0blayout-alist
         (assq-delete-all
-         (intern 0bl-current-layout) 0bl-layout-alist))
+         (intern 0blayout-current) 0blayout-alist))
 
   ;; Add current layout to list
-  (add-to-list '0bl-layout-alist
-               (cons (intern 0bl-current-layout) (current-window-configuration)))
+  (add-to-list '0blayout-alist
+               (cons (intern 0blayout-current) (current-window-configuration)))
 
-  (message "Saved the currently active layout: %s" 0bl-current-layout))
+  (message "Saved the currently active layout: %s" 0blayout-current))
 
 
 
 ;;;###autoload
-(define-minor-mode 0bl-mode
+(define-minor-mode 0blayout
   "Handle layouts with ease"
-  :lighter " 0bl"
+  :lighter " 0bL"
   :global t
-  :group '0bl
+  :group '0blayout
   :keymap (let ((map (make-sparse-keymap)))
-            (define-key map (kbd "C-c C-l C-c") '0bl-new-layout)
-            (define-key map (kbd "C-c C-l C-k") '0bl-kill-layout)
-            (define-key map (kbd "C-c C-l C-b") '0bl-switch-layout)
+            (define-key map (kbd "C-c C-l C-c") '0blayout-new)
+            (define-key map (kbd "C-c C-l C-k") '0blayout-kill)
+            (define-key map (kbd "C-c C-l C-b") '0blayout-switch)
             map))
 
-(provide '0bl-mode)
+(provide '0blayout)
 
-;;; 0bl-mode.el ends here
+;;; 0blayout.el ends here
