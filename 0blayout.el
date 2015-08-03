cf019354169b2e8e03bb432d38af95c3244beee3
Steve Purcell <steve@sanityinc.com>
2015-08-04 07:45:02 +1200

    Use -mode prefix just for the minor mode

--- 0blayout.el
+++ 0blayout.el
@@ -13,7 +13,7 @@
 ;; the buffers you left open before you switched (unless you closed it).
 
 ;; It doesn't require any setup at all more than:
-;; (0blayout)
+;; (0blayout-mode)
 
 ;; When you start emacs with 0blayout loaded, you will have a default layout
 ;; named "default", and then you can create new layouts (C-c C-l C-c), switch
@@ -136,7 +136,7 @@
 
 
 ;;;###autoload
-(define-minor-mode 0blayout
+(define-minor-mode 0blayout-mode
   "Handle layouts with ease"
   :lighter " 0bL"
   :global t
